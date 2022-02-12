3cd78f42ac0999c650defa14c357202b8701dabf
Judit Serrano Espinosa <jse22@mscloud.ua.es>
2022-02-12 17:36:03 +0100

    Minutes2Seconds creado

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -10,13 +10,25 @@ namespace hada_p1
     {
         public static double Seconds2Minutes(double s)
         {
-            if (s==0)
+            if (s == 0)
             {
                 return 0;
             }
             else 
             {
                 return s / 60;
+            }
+        }
+
+        public static double Minutes2Seconds (double m)
+        {
+            if (m == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 60 * m;
             }
         }
     }
